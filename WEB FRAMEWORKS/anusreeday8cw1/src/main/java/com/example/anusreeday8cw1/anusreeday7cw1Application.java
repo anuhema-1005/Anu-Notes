b4bf1295f6adae6e3c9d4aab316a844bae4d367e
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2024-07-10 22:39:12 +0530

    day 6

---
+++ WEB FRAMEWORKS/anusreeday8cw1/src/main/java/com/example/anusreeday8cw1/anusreeday7cw1Application.java
@@ -0,0 +1,13 @@
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class anusreeday7cw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(anusreeday7cw1Application.class, args);
+	}
+
+}
---
+++ WEB FRAMEWORKS/anusreeday8cw2/src/main/java/com/example/anusreeday8cw2/anusreeday7cw2Application.java
@@ -0,0 +1,13 @@
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class anusreeday7cw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(anusreeday7cw2Application.class, args);
+	}
+
+}
