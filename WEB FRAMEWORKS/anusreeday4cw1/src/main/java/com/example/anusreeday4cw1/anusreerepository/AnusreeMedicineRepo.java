171e71a8cc273ffec8b196f9bca5d6e7eeae0b15
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2024-07-10 22:46:08 +0530

    Delete WEB FRAMEWORKS directory

--- WEB FRAMEWORKS/anusreeday4cw1/src/main/java/com/example/anusreeday4cw1/anusreerepository/AnusreeMedicineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.anusreeday4cw1.anusreerepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.anusreeday4cw1.anusreemodel.AnusreeMedicine;
-
-public interface AnusreeMedicineRepo extends JpaRepository<AnusreeMedicine,Integer>{
-    
-}
--- WEB FRAMEWORKS/anusreeday5cw3/src/main/java/com/example/anusreeday5cw3/anusreerepository/AnusreeLanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5cw3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5cw3.model.AnusreeLanguage;
-
-public interface AnusreeLanguageRepo extends JpaRepository<AnusreeLanguage,Integer>{
-    
-}
