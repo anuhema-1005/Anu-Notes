d270927c77d11a0848bae68fb5779e4272159bf1
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2024-07-10 22:40:44 +0530

    day 7

---
+++ WEB FRAMEWORKS/anusreeday10pah/src/main/java/com/example/anusreeday10pah/anusreeday10pahpplication.java
@@ -0,0 +1,13 @@
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class anusreeday10pahpplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(anusreeday10pahpplication.class, args);
+	}
+
+}
---
+++ WEB FRAMEWORKS/class_exercise_2/anusreeday9cw2/src/main/java/com/example/anusreeday9cw2/anusreecontroller/anusreeRecipeController.java
@@ -0,0 +1,50 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.anusreeRecipe;
+import com.example.springapp.service.anusreeRecipeService;
+
+@RestController
+public class anusreeRecipeController {
+    @Autowired
+    private anusreeRecipeService ser;
+
+    @PostMapping("/api/recipe")
+    public ResponseEntity<anusreeRecipe> post(@RequestBody anusreeRecipe person) {
+        if (ser.post(person)) {
+            return new ResponseEntity<>(person, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/recipe/byname")
+    public ResponseEntity<List<anusreeRecipe>> getAllbyFiltering(@RequestParam("recipeName") String recipeName) {
+        List<anusreeRecipe> li = ser.filter(recipeName);
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/recipe/{recipeId}")
+    public ResponseEntity<anusreeRecipe> getbyrecipeId(@PathVariable("recipeId") int recipeId) {
+        anusreeRecipe li = ser.getByid(recipeId);
+        if (li!=null) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
