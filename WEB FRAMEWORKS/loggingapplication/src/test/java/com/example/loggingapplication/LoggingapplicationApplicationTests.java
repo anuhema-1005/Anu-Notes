45a1c3df9a02695ea774be54e9572581e6ce9674
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2024-07-10 22:28:05 +0530

    Web frameworks

---
+++ WEB FRAMEWORKS/loggingapplication/src/test/java/com/example/loggingapplication/LoggingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
