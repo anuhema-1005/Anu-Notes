7a8a5aa029fd0b4eeb910f1e601d0f792f81a33e
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2024-07-10 21:19:17 +0530

    Application Development Practices

---
+++ APPLICATION DEVELOPMENT PRACTICES/Calculator.js
@@ -0,0 +1,18 @@
+var a= parseInt(prompt("Enter first value"));
+var b= parseInt(prompt("Enter second value"));
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+document.getElementById("sum").innerHTML=c;
+document.getElementById("sub").innerHTML=d;
+document.getElementById("mul").innerHTML=e;
+document.getElementById("div").innerHTML=f;
+document.writeln(c);
+document.write("<br>")
+document.writeln(d);  
+document.write("<br>")
+document.writeln(e);  
+document.write("<br>")
+document.writeln(f);
+document.write("<br>")
